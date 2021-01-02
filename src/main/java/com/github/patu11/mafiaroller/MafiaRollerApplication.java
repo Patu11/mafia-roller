ff08517e11a3c90fe2c5291c0be438df2b0255ce
Patryk <patry07@o2.pl>
2021-01-02 21:02:47 +0100

    added spring project

---
+++ src/main/java/com/github/patu11/mafiaroller/MafiaRollerApplication.java
@@ -0,0 +1,13 @@
+package com.github.patu11.mafiaroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaRollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafiaRollerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/patu11/mafiaroller/MafiaRollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.patu11.mafiaroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaRollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
